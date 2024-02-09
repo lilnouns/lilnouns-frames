b06370976f13bc3da6bf227999f2676fedaf6d40
Milad Nekofar <milad@nekofar.com>
2024-02-09 18:28:06 +0330

    feat: add `checkENSExists` function to check ens availability

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,29 @@
 export type FrameSignaturePacket = {
   untrustedData: {
-    fid: number;
-    url: string;
-    messageHash: string;
-    timestamp: number;
-    network: number;
-    buttonIndex: number;
-    inputText?: string;
+    fid: number
+    url: string
+    messageHash: string
+    timestamp: number
+    network: number
+    buttonIndex: number
+    inputText?: string
     castId: {
-      fid: number;
-      hash: string;
-    };
-  };
+      fid: number
+      hash: string
+    }
+  }
   trustedData: {
-    messageBytes: string;
-  };
-};
+    messageBytes: string
+  }
+}
+
+export interface GraphQLEnsDomainResponse {
+  data: {
+    domains: Array<Domain>
+  }
+}
+
+interface Domain {
+  id: string
+  name: string
+}
