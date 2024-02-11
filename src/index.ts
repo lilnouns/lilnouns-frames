6fccbc4c8a2e5b1cac79b394e0f243c0b607dd37
Milad Nekofar <milad@nekofar.com>
2024-02-11 14:39:37 +0330

    feat: create domains routes for check subdomain availability

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import { Hono } from 'hono'
-import { hello } from './routes'
+import { logger } from 'hono/logger'
+import { domains, hello } from './routes'
 import { Environment } from './types'
 
 const app = new Hono<Environment>()
 
+app.use(logger())
+
 app.route('/hello', hello)
+
+app.route('/domains', domains)
 
 export default app
